91a299b10587ed168cd591e6af9a8c9e0918b32a
RPraxedes <richard0praxedes@gmail.com>
2019-08-30 13:56:18 +0800

    added audit log status badges

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -13,6 +13,10 @@ module PostsHelper
           content_tag(:span, status.titleize, class: 'badge badge-success')
         when 'rejected'
           content_tag(:span, status.titleize, class: 'badge badge-danger')
+        when 'pending'
+          content_tag(:span, status.titleize, class: 'badge badge-warning')
+        when 'confirmed'
+          content_tag(:span, status.titleize, class: 'badge badge-success')
       end
     end
 end
