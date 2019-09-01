6c79779b5febe78e3a5fdb5ffc3afbdad13c995f
RPraxedes <RPraxedes@gmail.com>
2019-09-01 12:34:21 +0800

    added buttons for audit confirmations

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -13,4 +13,14 @@ describe 'Homepage' do
 
     expect(post.reload.status).to eq('approved')
   end
+
+  it 'allows the employee to change the audit log status from the homepage' do
+    user = FactoryGirl.create(:user)
+    audit_log = FactoryGirl.create(:audit_log)
+    login_as(admin_user, :scope => :user)
+    audit_log.update(user_id: user.id)
+    visit root_path
+    click_on("confirm_#{audit_log.id}")
+
+    expect(audit_log.reload.status).to eq('confirmed')
 end
