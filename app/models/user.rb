cd4ef343396c593eac6971e8b99cb17e54e724b9
RPraxedes <RPraxedes@gmail.com>
2019-09-01 17:52:40 +0800

    added self referential model for user manager/employee association

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :posts
   has_many :audit_logs
+  has_many :hands_associations, class_name: 'Hand'
+  has_many :hands, through: :hands_associations
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates_presence_of :email, :first_name, :last_name
