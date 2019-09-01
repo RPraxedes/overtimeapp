0cb96db4a7d8041e0cb7183634f35df73151452f
RPraxedes <RPraxedes@gmail.com>
2019-09-01 16:52:05 +0800

    updated seed to generate employees

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-@user = User.create(email: "test@test.com", password: "asdfasdf",
-  password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+@user = Employee.create(email: "test@test.com", password: "asdfasdf",
+  password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow",)
 
 AdminUser.create(email: "admin@test.com", password: "password",
   password_confirmation: "password", first_name: "Admin", last_name: "User")
