641e12341772091720c747d16413922ee59f7f2f
RPraxedes <RPraxedes@gmail.com>
2019-09-01 13:25:18 +0800

    updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,15 +12,15 @@
 AdminUser.create(email: "admin@test.com", password: "password",
   password_confirmation: "password", first_name: "Admin", last_name: "User")
 
-100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user: @user, overtime_request: 2.5)
-end
-
-puts "100 posts have been created."
-
 AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
 AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
 AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
 
 
 puts "3 audit logs have been created."
+
+100.times do |post|
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user: @user, overtime_request: 2.5)
+end
+
+puts "100 posts have been created."
